88b176751eac9b41bb625717bbdeb77cbca7557e
John Turner <john@seedprod.com>
2015-02-28 14:50:55 -0500

    u

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -avz --delete --size-only _site/ root@104.236.30.213:/var/www/html
+rsync -rtzhv --delete --size-only _site/ root@104.236.30.213:/var/www/html
 ssh root@104.236.30.213 'chown -R www-data:www-data /var/www/html/'
